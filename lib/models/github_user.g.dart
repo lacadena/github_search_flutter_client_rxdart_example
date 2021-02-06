953da2505edb62437a8f873facf7ab1726195eea
Karoll Dayana <karollcontreras@outlook.es>
2021-02-06 17:03:17 -0500

    <1> <Home_page_improvements> <karollDayana>

---
+++ lib/models/github_user.g.dart
@@ -0,0 +1,22 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'github_user.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_GitHubUser _$_$_GitHubUserFromJson(Map<String, dynamic> json) {
+  return _$_GitHubUser(
+    login: json['login'] as String,
+    avatarUrl: json['avatarUrl'] as String,
+    htmlUrl: json['htmlUrl'] as String,
+  );
+}
+
+Map<String, dynamic> _$_$_GitHubUserToJson(_$_GitHubUser instance) =>
+    <String, dynamic>{
+      'login': instance.login,
+      'avatarUrl': instance.avatarUrl,
+      'htmlUrl': instance.htmlUrl,
+    };
